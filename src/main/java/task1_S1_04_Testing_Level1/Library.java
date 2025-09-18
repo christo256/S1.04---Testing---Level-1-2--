d198616176f4562d35a21be7712a82fd0dba58eb
christo256 <ckingmaster2@gmail.com>
2025-09-18 13:06:29 +0200

    creating class Library.java

---
+++ src/main/java/task1_S1_04_Testing_Level1/Library.java
@@ -0,0 +1,15 @@
+package task1_S1_04_Testing_Level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<String> books;
+
+    public Library (){
+        books = new ArrayList<>();
+
+    }
+
+}
