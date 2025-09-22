5136497cfb6df5aa469feb6ffb48c7a71ed3064c
christo256 <ckingmaster2@gmail.com>
2025-09-22 13:59:25 +0200

    adding Dni's for try tessting

--- src/test/java/task2_S1_04_Testing_Level1/CalculateDniTest.java
+++ src/test/java/task2_S1_04_Testing_Level1/CalculateDniTest.java
@@ -1,4 +1,16 @@
 package task2_S1_04_Testing_Level1;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculateDniTest {
+    @ParameterizedTest
+    @CsvSource({
+            "12345678, Z",
+
+    }
+
+
+
+
 }
