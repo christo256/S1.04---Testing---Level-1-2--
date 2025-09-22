4b898b6f82fd223c43640193a066f6896d030dfe
christo256 <ckingmaster2@gmail.com>
2025-09-22 13:52:45 +0200

    Implement CalculateDni class with character array and add parameterized tests for DNI calculation

---
+++ src/main/java/task2_S1_04_Testing_Level1/CalculateDni.java
@@ -0,0 +1,16 @@
+package task2_S1_04_Testing_Level1;
+
+public class CalculateDni {
+
+    public static char calculateDni(int dniNumber) {
+        if (dniNumber < 0 || dniNumber > 99999999) {
+            throw new IllegalArgumentException("Invalid DNI number");
+        }
+        char[] lettersDNI = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        int letter = dniNumber % 23;
+        return lettersDNI[letter];
+    }
+
+
+}
---
+++ src/test/java/task2_S1_04_Testing_Level1/CalculateDniTest.java
@@ -0,0 +1,4 @@
+package task2_S1_04_Testing_Level1;
+
+public class CalculateDniTest {
+}
