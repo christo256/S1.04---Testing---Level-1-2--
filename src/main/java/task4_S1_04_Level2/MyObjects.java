f645f13fe77928346bb6d2b2d07c23f510f2f1ef
christo256 <ckingmaster2@gmail.com>
2025-09-29 11:36:22 +0200

    feat: create MyObject class to use in collection tests

---
+++ src/main/java/task4_S1_04_Level2/MyObjects.java
@@ -0,0 +1,38 @@
+package task4_S1_04_Level2;
+
+public class MyObjects {
+    private String name;
+
+    public MyObjects(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("name can`t be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof MyObjects)) return false;
+
+        MyObjects myObjects = (MyObjects) obj;
+        return this.name.equals(myObjects.name);
+    }
+        @Override
+    public int hashCode() {
+        return name.hashCode();
+
+        }
+
+    @Override
+    public String toString() {
+        return "MyObjects{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
